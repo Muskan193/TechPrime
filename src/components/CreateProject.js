75ac6278cc3e7b51f9bcc807ccf939b2e5e84b3a
Muskan193 <120629858+Muskan193@users.noreply.github.com>
2023-04-12 00:01:11 +0530

    login pagr

---
+++ src/components/CreateProject.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Header from './Header';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const CreateProject = () => {
+  return (
+    <>
+    <div className='main' >
+    <div class="row vertical-align">
+   <Header/>
+
+
+    <div className='content'>
+<div className='header'>
+<h1>Create Project</h1>
+</div>
+    </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default CreateProject
---
+++ src/components/Header.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import FormatListBulletedTwoToneIcon from '@mui/icons-material/FormatListBulletedTwoTone';
+import SpeedTwoToneIcon from '@mui/icons-material/SpeedTwoTone';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import Autocomplete from '@mui/material/Autocomplete';
+import LogoutIcon from '@mui/icons-material/Logout';
+import './Header.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink, useNavigate } from 'react-router-dom';
+// import CartBtn from './buttons/CartBtn';
+
+
+
+const Header = () => {
+  return (
+    <>
+     {/* <div className='sidebar'>
+    <ul >
+        <li><a href="#"><SpeedTwoToneIcon/></a></li>
+        <li><a href="#"><FormatListBulletedTwoToneIcon/></a></li>
+        <li><a href="#"><AddOutlinedIcon/></a></li>
+       
+       <li><a href="#"><LogoutIcon/></a></li>
+      </ul>
+     </div></> */}
+      <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll ">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <NavLink style={{ textDecoration: "none", fontWeight: 'bold', fontSize: '19px', color: "black", margin: '10px' }} to="/dashboard"><SpeedTwoToneIcon/></NavLink>
+            <NavLink style={{ textDecoration: "none", fontWeight: 'bold', fontSize: '19px', color: "black", margin: '10px' }} to="/pagelisting"><FormatListBulletedTwoToneIcon/></NavLink>
+            <NavLink style={{ textDecoration: "none", fontWeight: 'bold', fontSize: '19px', color: "black", margin: '10px' }} to="/craetepage"><AddOutlinedIcon/></NavLink>
+        
+          </Nav>
+          
+          
+          <NavLink to="/"   >
+            <LogoutIcon />    
+          </NavLink >
+          {/* <CartBtn /> */}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+     </>
+     
+  )
+}
+
+export default Header
